a8939d3eaa155bafb6765b513a2b1a063125b19c
Ahmed <hunterxhuntert9o@gmail.com>
2018-02-10 15:16:36 +0200

    Initial Commit

---
+++ accounts/admin.py
@@ -0,0 +1,78 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import *
+
+
+class UserCreationForm(forms.ModelForm):
+    
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        
+    def clean_password2(self):
+        
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        
+        return password2
+
+    def save(self, commit=True):
+        
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        
+        if commit:
+            user.save()
+            
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+
+    password = ReadOnlyPasswordHashField()
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'is_active', 'is_admin')
+
+    def clean_password(self):
+
+        return self.initial["password"]
+
+
+class UserAdmin(BaseUserAdmin):
+    
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'email', 'is_admin', 'is_staff')
+    list_filter = ('is_admin',)
+    fieldsets = (
+    (None, {'fields': ('username', 'first_name', 'middle_name',
+                       'last_name', 'password'
+                       )}),
+    ('Personal info', {'fields': ('email',)}),
+    ('Permissions', {'fields': ('is_admin','is_staff', 'is_active')}),
+    )
+
+    add_fieldsets = (
+    (None, {
+    'classes': ('wide',),
+    'fields': ('username', 'password1', 'password2')}
+    ),
+    )
+    
+search_fields = ('username',)
+ordering = ('username',)
+filter_horizontal = ()
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
---
+++ accounts/models.py
@@ -0,0 +1,94 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, username, role, password=None):
+        
+        if not username:
+            raise ValueError('Please Enter a Username')
+            
+        user = self.model(
+            username=username,
+            role=role
+            )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, username, role, password):
+        
+        user = self.create_user(
+            username,
+            role=role,
+            password=password,
+            )
+        
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+
+
+class User(AbstractUser):
+    
+    id = models.AutoField(primary_key=True)
+    is_active = models.BooleanField(default=True)
+    is_suspended = models.BooleanField(default=False)
+    is_disabled = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    signed_in_once = models.BooleanField(default=False)
+    saw_walkthrough = models.BooleanField(default=False)
+    
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    
+    first_name = models.CharField(max_length=200, null=True, blank=True)
+    middle_name = models.CharField(max_length=200, null=True, blank=True)
+    last_name = models.CharField(max_length=200, null=True, blank=True)
+        
+    latest_contact = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+    
+    objects = UserManager()
+    
+    class Meta:
+        
+        ordering = ('first_name', 'last_name')
+        
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+    def as_dict(self):
+        
+        data = {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'full_name': self.full_name,
+            'email': self.email
+        }
+        
+        return data
+    
+    @property
+    def full_name(self):
+        
+        if self.first_name and self.last_name:
+            return '{} {}'.format(self.first_name, self.last_name)
+        if self.first_name:
+            return self.first_name
+        if self.last_name:
+            return self.last_name
+        
+        return 'N/A'
