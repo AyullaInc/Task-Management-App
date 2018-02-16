fa634ba8950d596d9ce1516263c6e46356d9bc8d
Manuj jain <32789683+manujjain@users.noreply.github.com>
2018-02-16 22:25:34 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,5 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render
 
-
-
-def home(request):
-	    return render(request, 'home.html', {})
-
-def team_signup(request):
-
-	    return render(request, 'team_signup.html', {})
-
-def login(request):
-	    return render(request, 'login.html', {})
+#create your views here
 
 
