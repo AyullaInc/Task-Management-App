302611ac32bbf02fff3d7556ec704da3d748d57c
Manuj jain <32789683+manujjain@users.noreply.github.com>
2018-02-17 00:24:34 +0530

    Update urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
 from django.conf.urls import url,include
 
 
@@ -22,8 +22,8 @@ from django.conf.urls import url,include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^',include('accounts.urls')),
 
     
-
+]
