da771b30b65ed744cfe657a6fe87fa5f00823b59
Manuj jain <32789683+manujjain@users.noreply.github.com>
2018-02-16 22:23:37 +0530

    Delete urls.py

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url,include
-from django.contrib import admin
-from .views import home,team_signup,login
-
-urlpatterns = [
-    url(r'^$',home),
-    url(r'^create-team$',team_signup),
-    url(r'^login$',login),
-]
